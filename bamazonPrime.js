5e5122cc4bdbd5cc35945a5edc99a1676c246b3c
JOSH-ROBERTS-PC\joshr <josro14@gmail.com>
2018-04-25 18:12:12 -0600

    sql var and connection var
    established basic connection to sql database

---
+++ bamazonPrime.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Jpr8353!",
+    database: "top_songsdb"
+  });
